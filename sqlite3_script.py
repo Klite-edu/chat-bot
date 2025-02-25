75448f70ec55cb50e6140eadb6dc0ab470dd1593
MishraAbhishek07 <davmishra96@gmail.com>
2025-02-25 15:29:37 +0530

    Chat working but not with Django

--- sqlite3_script.py
+++ sqlite3_script.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 # Connect to database
 conn = sqlite3.connect("chats.db")
 cursor = conn.cursor()
@@ -7,19 +8,40 @@ cursor = conn.cursor()
 # Create table
 # cursor.execute("""
 #     CREATE TABLE IF NOT EXISTS chats (
-#         chat TEXT
+#         chat TEXT,
+#         user_id INTEGER,
+#         role TEXT
 #     )
 # """)
 # conn.commit()
 
-# Insert data
-# cursor.execute("INSERT INTO chats (chat) VALUES (?)", ("Alice",))
+# # Deleting table
+# cursor.execute("""
+#     DROP TABLE chats
+# """)
 # conn.commit()
 
-# Fetch data
-cursor.execute("SELECT * FROM chats")
-users = cursor.fetchall()
-print("Users in database:", users)
+
+# View table structure / schema
+# tables = cursor.fetchall()
+# for table in tables:
+#     print(table[0])
+
+# Insert data
+# cursor.execute("INSERT INTO chats (chat, user_ID, role) VALUES (?, ?, ?)", ("Alice", "Bot", "User/Bot", ))
+# conn.commit()
+
+# cursor.execute("SELECT * FROM chats")
+# users = cursor.fetchall()
+# print(users)
+# conn.commit()
+# # # Fetch data
+# def test(user_id):
+#     cursor.execute("SELECT * FROM chats")
+#     users = cursor.fetchall()
+#     for i in users:
+#         if i[2] == 'user' and i[1] == user_id:
+#             print(i)
 
 # Update data
 # cursor.execute("UPDATE users SET age = ? WHERE name = ?", (26, "Alice"))
